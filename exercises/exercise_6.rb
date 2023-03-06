ccf26e200ce0143b27cf245d63f6de95e1ed3595
dorreb <ashley_ure@hotmail.com>
2023-03-06 13:33:41 -0500

    add: create employees and link the stores and employees together

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Billy", last_name: "Jean", hourly_rate: 60)
+@store1.employees.create(first_name: "Jack", last_name: "Black", hourly_rate: 60)
+@store1.employees.create(first_name: "Jessie", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "Laura", last_name: "West", hourly_rate: 60)
+@store2.employees.create(first_name: "Sam", last_name: "North", hourly_rate: 60)
+@store1.employees.create(first_name: "Kelly", last_name: "Ridge", hourly_rate: 60)
